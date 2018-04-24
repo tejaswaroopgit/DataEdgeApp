e64f61c99b8664ce71ba4d720cac956c920fb4e8
tejaswaroopgit <30846434+tejaswaroopgit@users.noreply.github.com>
2018-04-24 15:38:01 -0400

    Search functionality fully working
    search functionality

--- client/partials/clients/searchCtrl.js
+++ client/partials/clients/searchCtrl.js
@@ -1,10 +1,23 @@
 myApp.factory('searchService', function ($http) {
     return {
-        searchConsultant: function (searchObj,success,error) {
-            console.log(searchObj.location);
+        searchConsultant: function (searchObj, success, error) {
+            console.log(typeof( searchObj.location) + typeof( searchObj.domain) + searchObj.visa);
             var location = searchObj.location;
             var domain = searchObj.domain;
             var visa = searchObj.visa;
+
+            if (location == undefined ) {
+                console.log("if location loop entered...!!")
+                location = "null";
+            }
+            if (domain == undefined) {
+                console.log("if domain loop entered...!!")
+                domain = "null";
+            }
+            if (visa == undefined ) {
+                console.log("if visa loop entered...!!")
+                visa = "null";
+            }
             $http({
                 url: '/consultants/searchConsultants/'+location +'/'+domain+'/'+visa,
                 method: "GET"
@@ -30,9 +43,17 @@ myApp.controller('searchCtrl', function ($scope, $location, consultantService, s
     console.log(searchObj)
 
     $scope.searchConsultants = function (searchObj) {
+
+        console.log(searchObj.location + searchObj.domain + searchObj.visa)
         searchService.searchConsultant(searchObj, function (success) {
             console.log(success.data)
+            $scope.searchLists = success.data;
         });
+        $scope.searchObj = {
+            domain: null,
+            location: null,
+            visa: null
+        }
     }
   
 
